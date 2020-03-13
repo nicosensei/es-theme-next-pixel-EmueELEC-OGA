4798fc745da729398531e2da77e4573931ba0bd4
NG Dev <ng.software.dev@gmail.com>
2020-03-13 15:08:36 +0100

    Scripts that converts the original theme

--- adapt_from_crt.sh
+++ adapt_from_crt.sh
@@ -1,7 +1,7 @@
 mv favorites auto-favorites
 cp -rf default setup
 cp -rf imageviewer auto-lastplayed
-find . -name "background.jpg" | while read f; do mogrify -verbose -gravity center -crop 1024x683+0+0 $f; done
+find . -name "background.png" | while read f; do mogrify -verbose -gravity center -crop 1024x683+0+0 $f; done
 find . -name "background_legacy.jpg" | while read f; do mogrify -verbose -gravity center -crop 1024x683+0+0 $f; done
 find . -name "background_wheel.jpg" | while read f; do mogrify -verbose -gravity center -crop 1024x683+0+0 $f; done
 find . -name "background_gamelist_*" | while read f; do mogrify -verbose -gravity center -crop x683+0+0 $f; done
